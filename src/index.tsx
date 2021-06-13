141474c5cb2745bbb1b8f3aba496e8adb954f7f5
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-06-13 19:01:53 +0200

    added CodeCell component

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,11 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { useState } from 'react';
 import ReactDOM from 'react-dom';
-import { CodeEditor, Preview } from './components';
-import bundle from './bundler';
+import { CodeCell } from './components';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
-
   return (
     <div>
-      <CodeEditor
-        initialValue='const a = 1;'
-        onChange={(value) => setInput(value)}
-      />
-      <div>
-        <button onClick={onClick}>Submit</button>
-      </div>
-      <Preview code={code} />
+      <CodeCell />
     </div>
   );
 };

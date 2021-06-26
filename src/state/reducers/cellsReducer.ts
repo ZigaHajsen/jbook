455107356640786bc13104828694414590667921
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-06-26 17:19:24 +0200

    fixed typing

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -19,52 +19,54 @@ const initialState: CellsState = {
   data: {},
 };
 
-const reducer = produce((state: CellsState = initialState, action: Action) => {
-  switch (action.type) {
-    case ActionType.UPDATE_CELL:
-      const { id, content } = action.payload;
-      state.data[id].content = content;
-      return state;
-    case ActionType.DELETE_CELL:
-      delete state.data[action.payload];
-      state.order = state.order.filter((id) => id !== action.payload);
-      return;
-    case ActionType.MOVE_CELL:
-      const { direction } = action.payload;
-      const index = state.order.findIndex((id) => id === action.payload.id);
-      const targetIndex = direction === 'up' ? index - 1 : index + 1;
-
-      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+const reducer = produce(
+  (state: CellsState = initialState, action: Action): CellsState => {
+    switch (action.type) {
+      case ActionType.UPDATE_CELL:
+        const { id, content } = action.payload;
+        state.data[id].content = content;
         return state;
-      }
+      case ActionType.DELETE_CELL:
+        delete state.data[action.payload];
+        state.order = state.order.filter((id) => id !== action.payload);
+        return state;
+      case ActionType.MOVE_CELL:
+        const { direction } = action.payload;
+        const index = state.order.findIndex((id) => id === action.payload.id);
+        const targetIndex = direction === 'up' ? index - 1 : index + 1;
 
-      state.order[index] = state.order[targetIndex];
-      state.order[targetIndex] = action.payload.id;
-      return state;
-    case ActionType.INSERT_CELL_BEFORE:
-      const cell: Cell = {
-        id: randomId(),
-        type: action.payload.type,
-        content: '',
-      };
+        if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+          return state;
+        }
 
-      state.data[cell.id] = cell;
+        state.order[index] = state.order[targetIndex];
+        state.order[targetIndex] = action.payload.id;
+        return state;
+      case ActionType.INSERT_CELL_BEFORE:
+        const cell: Cell = {
+          id: randomId(),
+          type: action.payload.type,
+          content: '',
+        };
 
-      const foundIndex = state.order.findIndex(
-        (id) => id === action.payload.id
-      );
+        state.data[cell.id] = cell;
 
-      if (foundIndex < 0) {
-        state.order.push(cell.id);
-      } else {
-        state.order.splice(foundIndex, 0, cell.id);
-      }
+        const foundIndex = state.order.findIndex(
+          (id) => id === action.payload.id
+        );
 
-      return state;
-    default:
-      return state;
+        if (foundIndex < 0) {
+          state.order.push(cell.id);
+        } else {
+          state.order.splice(foundIndex, 0, cell.id);
+        }
+
+        return state;
+      default:
+        return state;
+    }
   }
-});
+);
 
 const randomId = () => {
   return Math.random().toString(36).substr(2, 5);

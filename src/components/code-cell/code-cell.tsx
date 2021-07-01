66fb0c3829b2abd26e523f6e6005bc08a6c18150
Žiga Hajšen <ziga.hajsen@gmail.com>
2021-07-01 12:11:32 +0200

    added loading

--- src/components/code-cell/code-cell.tsx
+++ src/components/code-cell/code-cell.tsx
@@ -1,3 +1,4 @@
+import './code-cell.css';
 import { useEffect } from 'react';
 import { CodeEditor, Preview } from '..';
 import { Resizable } from '..';
@@ -44,7 +45,15 @@ export const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} err={bundle.err} />}
+        {!bundle || bundle.loading ? (
+          <div className='progress-cover'>
+            <progress className='progress is-small is-primary' max='100'>
+              Loading
+            </progress>
+          </div>
+        ) : (
+          <Preview code={bundle.code} err={bundle.err} />
+        )}
       </div>
     </Resizable>
   );
